33d5ee38713da30ef526bcf10772ed4973357ec4
Aditya Kumar <2005aditya.k@gmail.com>
2024-08-27 19:16:54 +0530

    day02 assignment

--- cpp/day02.cpp
+++ cpp/day02.cpp
@@ -66,7 +66,53 @@ int main(){
     else if(m>=70 && m<80){cout << "C";}
     else if(m>=60 && m<70){cout << "D";}
     else{cout<<"F";}
+    cout<<endl;
 
     //3.
-    
+    int x;
+    cout<<"Enter a number: ";
+    cin>>x;
+    cout<<"The month is: ";
+    switch(x){
+        case 1:
+        cout<<"January";
+        break;
+        case 2:
+        cout<<"February";
+        break;
+        case 3:
+        cout<<"March";
+        break;
+        case 4:
+        cout<<"April";
+        break;
+        case 5:
+        cout<<"May";
+        break;
+        case 6:
+        cout<<"June";
+        break;
+        case 7:
+        cout<<"July";
+        break;
+        case 8:
+        cout<<"August";
+        break;
+        case 9:
+        cout<<"September";
+        break;
+        case 10:
+        cout<<"August";
+        break;
+        case 11:
+        cout<<"Nevember";
+        break;
+        case 12:
+        cout<<"December";
+        break;
+        default:
+        cout<< "Invalid";
+        break;
+    }
+    cout>>endl;
 }
